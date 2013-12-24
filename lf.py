d2dac8eed2ba03afbfaaf77e90baeaafe5ee81cc
lchsk <mjlechowski@gmail.com>
2013-12-24 01:27:56 +0100

    # Removed unnecessary comments # Cleaning

--- lf.py
+++ lf.py
@@ -5,22 +5,12 @@ import message
 from twisted.internet.defer import Deferred
 import time
 
-'''
-1. Ustanowienie połączenia między klientem a serwerem
-2. Serwer wysyła SignInRequest
-3. Klient odsyła pakiet SignInResponse. W tym pakiecie znajduje się nazwa postaci jaką wybrał gracz (a postać wybiera się przed dołączeniem do gry).
-4. Jeśli serwer otrzymał SignInResponse od każdego klienta to wysyła pakiet CreateHeroes z informacjami na temat nazw postaci, ich peerID i pozycji.
-5. Klient tworzy graczy zgodnie z otrzymanym pakietem i odysła HeroesCreated.
-'''
-
 class LfProtocol(basic.LineReceiver):
 
     def connectionMade(self):
-        print "\n\nConnection from ", self.transport.getPeer().host
+        print "\n\nNew connection from", self.transport.getPeer().host
     
     def dataReceived(self, data):
-        print data
-        print time.strftime("%H:%M:%S")
         #print "ASCII value: ", ', '.join(str(ord(c)) for c in data)
         d = Deferred()
         d.addCallback(interpreter.interpret)
@@ -31,7 +21,7 @@ class LfServerFactory(protocol.ServerFactory):
 
     def __init__(self):
         self.protocol  = LfProtocol
-        self.users = {}
+        self.users = {} # holding username, platforms
         self.games = {} # list of currently played games
 
 if __name__ == "__main__":
--- message.py
+++ message.py
@@ -6,32 +6,41 @@ import datetime
 
 class MessageInterpreter(object):
     def __init__(self, factory):
+        
+        # Incoming string
         self.raw_message = ''
+        
+        # Incoming message in json format
         self.message = {}
+         
         self.factory = factory
+        
+        # Ending added to every outgoing string
         self.ending = ''
 
     def interpret(self, pack):
+        
+        # Load stuff
+        
         self.raw_message = pack[0]
         self.protocol = pack[1]
         self.message = json.loads(self.raw_message)
-        print self.message
-        print 'id: ' + str(self.message['id'])
-
-        # Welcome 
-        #{"id": "W", "username": "lchsk", "platform":"A"}
-        if self.message['id'] == 'W':
-            print self.message['username']
-            #print self.factory.users
-            
+        
+        ############
+        # Messages
+        ############
+        
+        # 10 NewConnection
+        if self.message['id'] == 10:
             self._data = {}
             self._data['platform'] = self.message['platform']
             self._data['client'] = pack[1]
             self.factory.users[self.message['username']] = self._data
-            #print self.factory.users
-            print 'Player ' + str(self.message['username']) + ' added...'
-            self.factory.users[self.message['username']]['client'].transport.write(
-                'You (' + str(self.message['username']) + ') joined the game.')
+            
+            # 13 ConnectionEstablished
+            return_msg = {'id' : 13 }
+            
+            self.factory.users[self.message['username']]['client'].transport.write(json.dumps(return_msg) + self.ending)
 
         # Message
         #{"id":"M", "username": "lchsk", "x": "12", "y": "100", "anim": "1"}
@@ -40,29 +49,20 @@ class MessageInterpreter(object):
                 if user != self.message['username']:
                     self.factory.users[user]['client'].transport.write(self.raw_message)    
 
-        # CreateGame Packet
-        #{\"id\": \"1\", \"admin\": \"admin\"}
+        # 1 CreateGame
         elif self.message['id'] == 1:
             admin = self.message['admin']
             dat = datetime.datetime.now()
             self.factory.games[admin] = { 'open' : True, 'date' : dat.strftime('%m/%d/%Y %H:%M:%S'), 'players': [admin] }
             
-            # GameCreated Packet
+            # 2 GameCreated
             return_msg = {'id' : 2, 'date' : dat.strftime('%m/%d/%Y %H:%M:%S') }
-            print return_msg
-
-            # send back to the admin
-            #for user in self.factory.users:
-            #    if user == self.message['admin']:
+            
             self.factory.users[admin]['client'].transport.write(json.dumps(return_msg) + self.ending)  
             
-        # GetOpenGames
+        # 3 GetOpenGames
         elif self.message['id'] == 3:
-            #print self.factory.games
             open_games = [ (admin, items) for admin, items in self.factory.games.iteritems() if items['open'] == True]
-            print open_games
-            print self.factory.games
-            print len(self.factory.games)
             
             ret_games = {}
             for index, game in enumerate(open_games):
@@ -70,33 +70,29 @@ class MessageInterpreter(object):
                 ret_games[index]['admin'] = game[0]
                 ret_games[index]['date'] = game[1]['date']
             
-            # OpenGameList
+            # 4 OpenGamesList
             return_msg = {'id' : 4}
             return_msg = dict(return_msg.items() + ret_games.items())
-            print json.dumps(return_msg)
             
             self.protocol.transport.write(json.dumps(return_msg) + self.ending)
         
-        # Signin
+        # 5 SignIn
         elif self.message['id'] == 5:
             admin = self.message['admin']
             user = self.message['user']
             
             self.factory.games[admin]['players'].append(user)
             
-            print self.factory.games
-            
-        # GetSignedInPlayers
+        # 7 GetSignedInPlayers
         elif self.message['id'] == 7:
             admin = self.message['admin']
             
+            # 8 SignedInPlayers
             return_msg = {'id' : 8, 'players' : self.factory.games[admin]['players']}
-            
-            print return_msg
 
             self.factory.users[admin]['client'].transport.write(json.dumps(return_msg) + self.ending)
             
-        # StartGame
+        # 9 StartGame
         elif self.message['id'] == 9:
             admin = self.message['admin']
             
@@ -104,11 +100,12 @@ class MessageInterpreter(object):
             
             self.create_heroes(admin)
         
+        # 11 IsGameStarted
         elif self.message['id'] == 11:
             admin = self.message['admin']
             
             if self.factory.games[admin]['open']:
-                # game has not started
+                # 12 GameNotStarted
                 return_msg = {'id' : 12}
                 self.protocol.transport.write(json.dumps(return_msg) + self.ending)
             else:
@@ -116,21 +113,14 @@ class MessageInterpreter(object):
                 self.create_heroes(admin)
 
     def error(self, pack):
-        print 'Error while interpreting'
+        print 'Error...'
         traceback.print_exc(file=sys.stdout)
         
+    # 6 CreateHeroes
     def create_heroes(self, admin):
-        # send 6 to everyone
-            
+           
         return_msg = {'id' : 6, 'players' : self.factory.games[admin]['players']}
-        print 'Create Heroes'
-        print return_msg
-        
-        print 'Users'
-        print self.factory.users
         
         for user in self.factory.users:
             self.factory.users[user]['client'].transport.write(json.dumps(return_msg) + self.ending)
-
-    def test(self):
-        print 'test'
+            
