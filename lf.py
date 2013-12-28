048ddbdd1ddb9692e910a76186c6d56aae1ec85b
lchsk <mjlechowski@gmail.com>
2013-12-28 14:35:49 +0100

    # Logger

--- lf.py
+++ lf.py
@@ -9,9 +9,12 @@ class LfProtocol(basic.LineReceiver):
 
     def connectionMade(self):
         print "\n\nNew connection from", self.transport.getPeer().host
+        factory.logfile.write(time.strftime("%m/%d/%Y %H:%M:%S") + '\t' + 'New connection from ' + str(self.transport.getPeer().host) + '\n')
     
     def dataReceived(self, data):
         #print "ASCII value: ", ', '.join(str(ord(c)) for c in data)
+        print repr(data)
+        factory.logfile.write(time.strftime("%m/%d/%Y %H:%M:%S") + '\t' + repr(data) + '\n')
         d = Deferred()
         d.addCallback(interpreter.interpret)
         d.addErrback(interpreter.error)
@@ -23,6 +26,7 @@ class LfServerFactory(protocol.ServerFactory):
         self.protocol  = LfProtocol
         self.users = {} # holding username, platforms
         self.games = {} # list of currently played games
+        self.logfile = open(time.strftime("%m_%d_%Y_%H_%M_%S") + '.txt', 'w+')
 
 if __name__ == "__main__":
 
@@ -33,3 +37,5 @@ if __name__ == "__main__":
     reactor.listenTCP(port, factory)
     print "Server running since " + time.strftime("%m/%d/%Y %H:%M:%S") + '...'
     reactor.run()
+    
+    factory.logfile.close()
